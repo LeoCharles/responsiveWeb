24328bfaed032a029d8e6c1926cda442d757ccec
leofe <leofe200@163.com>
2017-10-18 21:39:27 +0800

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by leofe on 2017/9/25.
+ */
+$(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000
+    });
+});
